ddf74eae698ea912976f7bb65c4d28ca968d6f0d
Sheldon Codling <codlinsh@gmail.com>
2025-10-10 23:41:24 -0400

    Added string literal back to subject

--- netlify/functions/stripe-webhook.mjs
+++ netlify/functions/stripe-webhook.mjs
@@ -85,7 +85,7 @@ export async function handler(event) {
             body: JSON.stringify({
                 from: "onboarding@resend.dev",
                 to: email,
-                subject: `Thanks REPLACE WITH LITERAL — we received your booking!`,
+                subject: `Thanks ${fullName} — we received your booking!`,
                 html: cxHTML
             })
         });
